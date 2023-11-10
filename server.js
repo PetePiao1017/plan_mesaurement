a259866a707b2c3d15f5cd00b30b31880ea4ae1d
Richard Moore <petepiao1017@gmail.com>
2023-11-10 16:00:32 +0000

    updated

--- server.js
+++ server.js
@@ -32,6 +32,18 @@ require("./app/routes/user.routes")(app);
 require("./app/routes/category.routes")(app);
 require("./app/routes/measure.routes")(app);
 
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('./build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
 // set port, listen for requests
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
